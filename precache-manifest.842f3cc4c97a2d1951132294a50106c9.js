bcfe4ecf4d25aa8fd1921118d12922d2baef5c24
Emanuel <emanuelfontenla@gmail.com>
2020-06-22 21:06:23 -0300

    Updates

---
+++ precache-manifest.842f3cc4c97a2d1951132294a50106c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dca02a0ace4e24e5d17c9883c20bdf1",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef3e5e0f93dfc648073",
+    "url": "/react-deploy/static/js/main.31b7cdb8.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  }
+]);
